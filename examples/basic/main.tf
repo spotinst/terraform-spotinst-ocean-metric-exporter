a37349fbe1487a6435960b4a0ba56ee03fd3123d
sharadkesarwani <sharad.kesarwani@netapp.com>
2023-02-16 13:57:51 +0530

    updated latest image tag
    updated latest image tag

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -7,6 +7,6 @@ provider "helm" {
 module "ocean-metric-exporter" {
   source            = "../.."
   image_pull_policy = "Always"
-  image_tag         = "1.0.0-beta"
+  image_tag         = "1.0.2"
   namespace = "kube-system"
 }
